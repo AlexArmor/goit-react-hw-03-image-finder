f6fbdec1fd4ef3b9e155e63f7d65bade7457f519
Oleksandr Bron <lexlutermotion@gmail.com>
2022-12-22 20:34:58 +0200

    work on search form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
 import { Component } from 'react';
+import { Searchbar } from './Searchbar';
 
 export class App extends Component {
+  state = {
+    query: '',
+    images: [],
+    largeImageURL: '',
+    page: 1,
+    showBtn: false,
+  };
+
+  onFormSubmit = () => {};
+
   render() {
     return (
       <>
-        <h1>World</h1>
-        <div>Hello</div>
+        <Searchbar onFormSubmit={this.onFormSubmit} btnSearch={btnSearch} />
+        {/* <Loader />
+        <ImageGallery />
+        <Button />
+        <Modal /> */}
       </>
     );
   }
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+
+export class Searchbar extends Component {
+  state = {
+    searchValue: '',
+  };
+  render() {
+    return <input>Searchbar</input>;
+  }
+}
