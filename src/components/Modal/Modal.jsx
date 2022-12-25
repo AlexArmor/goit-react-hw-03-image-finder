49c56c2c8ce2f3dcde6724462ab7867f8fde4446
AlexArmor <lexlutermotion@gmail.com>
2022-12-25 15:07:55 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,14 @@
 import { ModalBackdrop, ModalWindow } from './Modal.styled';
 
-export const Modal = ({ largeImageURL, largeImageOpen, alt }) => {
+export const Modal = ({ largeImageURL, onImageClick, alt }) => {
+  const handleBackdrop = event => {
+    if (event.target === event.currentTarget) {
+      onImageClick('');
+    }
+  };
+
   return (
-    <ModalBackdrop>
+    <ModalBackdrop onClick={handleBackdrop}>
       <ModalWindow>
         <img src={largeImageURL} alt={alt} />
       </ModalWindow>
